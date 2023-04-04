2b964935807f3d536d5d3684365cce2755994b7b
Hariharan8Rpc <hh8rpc@gmail.com>
2023-04-04 14:34:06 +0530

    account finder update

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,12 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire,api } from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
 
 export default class AccountFinder extends LightningElement {
-    annualRevenue=null;
- 
+    @api annualRevenue=null;
     handleChange(event){
     this.annualRevenue=event.detail.value;
   }
 
+  @wire(queryAccountsByRevenue,{annualRevenue:'$annualRevenue'})
+  accounts;
   reset(){
     this.annualRevenue=null;
   }
