ad6714a6c52dca35da877470f5e6edbcee377e3c
Hariharan8Rpc <hh8rpc@gmail.com>
2023-04-14 17:32:47 +0530

    ip map

---
+++ force-app/main/default/lwc/publishIp/publishIp.js
@@ -0,0 +1,53 @@
+import { LightningElement ,track} from 'lwc';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getIpDetail from '@salesforce/apex/ipStack.getIpInfo';
+export default class PublishIp extends LightningElement {
+ @track ipAddress;
+ @track list;
+ @track mapMarkers=[];
+    handleipChange(event) {
+        this.ipAddress = event.target.value;
+        console.log('ipaddress'+this.ipAddress);
+    }
+
+    handleButtonClick() {
+        getIpDetail({ ipAddress: this.ipAddress })
+            .then(result => {
+                console.log('IP List : ', result);    
+                this.list=result;
+                if (result) {
+                    console.log('IP List : ', result);                
+                    this.list=result;
+                    this.dispatchEvent(new ShowToastEvent({
+                      title: 'Done',
+                      message: 'Results fetched',
+                      variant: 'success'
+                  }));
+                  
+                 this.mapMarkers = [
+                    {
+                        location: {
+                            Latitude: this.list.Latitude,
+                            Longitude: this.list.Longitude,
+                        },
+                    },
+                ];
+
+                } else {
+                    throw new Error(' error .');
+                }
+            })
+            .catch(error => {
+                console.log('ip listavalues in result'+result);
+                console.log('Error Occured: ', error);
+                this.dispatchEvent(new ShowToastEvent({
+                    title: 'Error',
+                    message: 'An error occurred while fetching IP Detail Please try again later.',
+                    variant: 'error'
+                }));
+            });
+    }
+    
+   
+}
