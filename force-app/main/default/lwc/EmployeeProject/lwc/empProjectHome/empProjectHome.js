61643c5afab13bf3fbfcb8b80def69d76ee8b1e3
Hariharan8Rpc <hh8rpc@gmail.com>
2023-06-13 16:34:50 +0530

    project component added in lwc for nav

--- force-app/main/default/lwc/EmployeeProject/lwc/empProjectHome/empProjectHome.js
+++ force-app/main/default/lwc/EmployeeProject/lwc/empProjectHome/empProjectHome.js
@@ -1,8 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 	
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class EmpProjectHome extends LightningElement {
+    @track enableComponent;
 
-    
+    @track showEmployeeListPage=false;
+    @track showProjectListPage=false;
+    handleValueChange(event) {
+      this.enableComponent = event.detail;
+      selectOption();   
+    }
+    selectOption() {
+        switch (this.enableComponent) {
+          case 'employeeList':
+            this.showEmployeeListPage=true;
+            this.showProjectListPage=false;
+            break;
+          case 'projectList':
+            this.showEmployeeListPage=false;
+            this.showProjectListPage=true;
+            break;
+          default:
+            this.showEmployeeListPage=false;
+            this.showProjectListPage=false;
+            break;
+        }
+      }
+
 }
--- force-app/main/default/lwc/EmployeeProject/lwc/employeeProjectNav/employeeProjectNav.js
+++ force-app/main/default/lwc/EmployeeProject/lwc/employeeProjectNav/employeeProjectNav.js
@@ -1,47 +1,58 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 export default class EmployeeProjectNav extends NavigationMixin(LightningElement){
-    navigateToHome() {
-        this[NavigationMixin.Navigate]({
-          type: 'standard__namedPage',
-          attributes: {
-            pageName: 'c__EmpProjectHome'
-          }
-        });
-    }
 
-    navigateToEmployeeList() {
-        this[NavigationMixin.Navigate]({
-          type: 'standard__namedPage',
-          attributes: {
-            componentName: 'c__EmployeeList'
-          }
-        });
-    }
+      showEmployeeList() {
+        value='employeeList';
+        const valueChangeEvent = new CustomEvent('valuechange', { detail: value });
+        console.log("employeelist+++ "+valueChangeEvent);
+        this.dispatchEvent(valueChangeEvent);
+      }
+      showProjectList() {
+        value='projectList';
+        const valueChangeEvent = new CustomEvent('valuechange', { detail: value });
+        console.log("projectList +++ "+valueChangeEvent);
+        this.dispatchEvent(valueChangeEvent);
+      }
 
 
-    EmployeeList
-
-    navToHomeComponent() {
-        this[NavigationMixin.Navigate]({
-            type: 'standard__component',
-            attributes: {
-                componentName: 'c__EmpProjectHome'
-            }
-            // c__EmpProjectHome
-        });
-    }   navToEmpListComponent
+    // navigateToHome() {
+    //     this[NavigationMixin.Navigate]({
+    //       type: 'standard__namedPage',
+    //       attributes: {
+    //         pageName: 'c__EmpProjectHome'
+    //       }
+    //     });
+    // }
+    // navigateToEmployeeList() {
+    //     this[NavigationMixin.Navigate]({
+    //       type: 'standard__namedPage',
+    //       attributes: {
+    //         componentName: 'c__EmployeeList'
+    //       }
+    //     });
+    // }
+    // navToListComponent() {
+    //     this[NavigationMixin.Navigate]({
+    //         type: 'standard__webPage',
+    //         attributes: {
+    //          //   componentName: 'c__EmpProjectHome'
+    //          url: '/c/c__EmployeeList'
+    //         }
+    //         // c__EmpProjectHome
+    //     });
+    // }   navToEmpHomeComponent
     
-    navToEmpListComponent() {
-        const pageReference = {
-            type: 'standard__component',
-            attributes: {
-                componentName: 'c__EmployeeList' // Replace with the actual name of the target component
-            },
-        };
-        const navigationItem = NavigationMixin(pageReference);
-        this.dispatchEvent(new CustomEvent('navigate', { detail: navigationItem }));
-    }
+    // navToEmpListComponent() {
+    //     const pageReference = {
+    //         type: 'standard__component',
+    //         attributes: {
+    //             componentName: 'c__EmployeeList' // Replace with the actual name of the target component
+    //         },
+    //     };
+    //     const navigationItem = NavigationMixin(pageReference);
+    //     this.dispatchEvent(new CustomEvent('navigate', { detail: navigationItem }));
+    // }
     
 
 
