37ec075161a7cfe2a4f649a31190830caad46b5f
Hariharan8Rpc <hh8rpc@gmail.com>
2023-06-14 18:18:15 +0530

    table added

--- force-app/main/default/lwc/EmployeeProject/lwc/employeeList/employeeList.js
+++ force-app/main/default/lwc/EmployeeProject/lwc/employeeList/employeeList.js
@@ -1,17 +1,41 @@
-import { LightningElement } from 'lwc';
-	
+import { LightningElement,wire,track } from 'lwc';
+import getEmployees from '@salesforce/apex/employeeControllerLwc1.getEmployees';
 import { NavigationMixin } from 'lightning/navigation';
+import Name from '@salesforce/schema/Employee__c.Name';
+import Department from '@salesforce/schema/Employee__c.Department__c';
+import Address from '@salesforce/schema/Employee__c.Address__c';
+import { getListUi } from 'lightning/uiListApi';
+
+
 export default class EmployeeList extends NavigationMixin(LightningElement) {
-// Navigation to lightning component
-// navigateToLightningComponent() {
-//     this[NavigationMixin.Navigate]({
-//         "type": "standard__component",
-//         "attributes": {
-//             //Here customLabelExampleAura is name of lightning aura component
-//             //This aura component should implement lightning:isUrlAddressable
-//             "componentName": "c__customLabelExampleAura"
-//         }
-//     });
+@track employeeList;
+
+    //     @wire(getListUi, { objectApiName: OBJECT_API_NAME, listViewApiName: null })
+// objectRecords;
+// get columns() {
+//     if (this.objectRecords.data) {
+//         console.log('lods of th data table'+this.objectRecords.data);
+//         return FIELD_API_NAMES.map(fieldApiName => ({
+           
+//             label: this.objectRecords.data.objectInfos[OBJECT_API_NAME].fields[fieldApiName].label,
+//             fieldName: fieldApiName,
+//             type: this.objectRecords.data.objectInfos[OBJECT_API_NAME].fields[fieldApiName].dataType
+//         }));
+//     }
+//     return [];
 // }
 
+// get rows() {
+//     if (this.objectRecords.data) {
+//         return this.objectRecords.data.records.records.map(record => record.fields);
+//     }
+//     return [];
+// }
+@wire(getEmployees)
+  wiredgetEmployee({error,data}){    
+        if(data){
+            this.employeeList=JSON.stringify(data);
+            console.log('employee List form front end '+JSON.stringify(data));
+  }
+}
 }
