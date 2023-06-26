1d1f1ec528ad6657c636c3b77fb7fb5420838224
Hariharan8Rpc <hh8rpc@gmail.com>
2023-06-26 15:59:32 +0530

    addr edit not working

--- force-app/main/default/lwc/EmployeeProject/lwc/employeeList/employeeList.js
+++ force-app/main/default/lwc/EmployeeProject/lwc/employeeList/employeeList.js
@@ -31,27 +31,7 @@ handleNameChange(event){
   this.name = event.target.value;
   console.log(this.name);
 } 
-    //     @wire(getListUi, { objectApiName: OBJECT_API_NAME, listViewApiName: null })
-// objectRecords;
-// get columns() {
-//     if (this.objectRecords.data) {
-//         console.log('lods of th data table'+this.objectRecords.data);
-//         return FIELD_API_NAMES.map(fieldApiName => ({
-           
-//             label: this.objectRecords.data.objectInfos[OBJECT_API_NAME].fields[fieldApiName].label,
-//             fieldName: fieldApiName,
-//             type: this.objectRecords.data.objectInfos[OBJECT_API_NAME].fields[fieldApiName].dataType
-//         }));
-//     }
-//     return [];
-// }
-
-// get rows() {
-//     if (this.objectRecords.data) {
-//         return this.objectRecords.data.records.records.map(record => record.fields);
-//     }
-//     return [];
-// }
+  
 @wire(getEmployees)
   wiredgetEmployee({error,data}){    
         if(data){
